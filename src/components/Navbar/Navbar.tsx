b2208c7c2622d099cc8444f9a5e459525a35ff56
FabianaKaori <fabianakaori.93@gmail.com>
2023-04-14 22:32:41 -0300

    Navbar, container, logo em svg

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import './Navbar.module.css'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <ul>
+                <li className='teste'>Home</li>
+                <li>Campeões</li>
+                <li>Perfil</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
