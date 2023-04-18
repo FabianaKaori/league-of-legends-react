d5a972038bf6381b7e91447916f523bd51ca22b3
FabianaKaori <fabianakaori.93@gmail.com>
2023-04-18 02:37:03 -0300

    Navbar: concluído parte css, html

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,17 +5,49 @@ function Navbar() {
         <nav className={styles.navbar}>
             <div className={styles.logoGroup}>
                 <img className={styles.imgLogoRiotGames} src='src\assets\Home\Logo\mainLogoRiotFirst.svg' />
-                <img className={styles.imgLogoRiotGamesLetter} src='src\assets\Home\Logo\logoRiotCirculo.svg'/>
+                <img className={styles.mainNavArrowDown}  src='src\assets\Home\Logo\mainNavArrowDown.svg' />
+                <img className={styles.imgLogoRiotGamesLetter} src='src\assets\Home\Logo\svgexport-19.svg'/>
             </div>
             <ul className={styles.ulFirst}>
                 <li><a>O jogo</a></li>
                 <li><a>Campeões</a></li>
-                <li><a>Notícias</a></li>
+                <li>
+                    <a>
+                        Notícias 
+                        <img className={styles.mainNavArrowDown} src='src\assets\Home\Logo\mainNavArrowDown.svg'/>
+                    </a>
+                </li>
                 <li><a>Notas de atualizações</a></li>
-                <li><a>Comunidade</a></li>
-                <li><a>Esports</a></li>
-                <li><a>Universo</a></li>
-                <li><a>Criador de Avatar</a></li>
+                <li>
+                    <a>
+                        Comunidade 
+                        <img className={styles.mainNavArrowDown} src='src\assets\Home\Logo\mainNavArrowDown.svg'/>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Esports 
+                        <img className={styles.mainNavLinkOut} src='src\assets\Home\Logo\mainNavLinkOut.svg'/>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Universo 
+                        <img className={styles.mainNavLinkOut} src='src\assets\Home\Logo\mainNavLinkOut.svg'/>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Suporte 
+                        <img className={styles.mainNavLinkOut} src='src\assets\Home\Logo\mainNavLinkOut.svg'/>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Mais 
+                        <img className={styles.mainNavArrowDown} src='src\assets\Home\Logo\mainNavArrowDown.svg'/>
+                    </a>
+                </li>
             </ul>
             <ul className={styles.ulSecond}>
                 <li><img className={styles.search} src='src\assets\Home\Logo\search.svg' /></li>
