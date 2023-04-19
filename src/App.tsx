69a30d61f7a82b2f3e6dfc9e7327bd5d3515a5db
FabianaKaori <fabianakaori.93@gmail.com>
2023-04-19 15:04:24 -0300

    Configuração router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer/Footer'
+
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
     <Navbar />
+    <Outlet />
     <Footer />
     </>
-  )
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import Home from './pages/Home'
+import Champion from './pages/Champion/Champion'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+
+import { createBrowserRouter, RouterProvider} from 'react-router-dom' 
+
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    
+    children:[
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/champions",
+        element: <Champion />,
+      },
+    ],
+  },
+
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
-)
+);
