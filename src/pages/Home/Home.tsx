f897e523978b8803a4458fa53bafc5e0d0787c3c
FabianaKaori <fabianakaori.93@gmail.com>
2023-04-20 16:34:49 -0300

    Home: criação de conteúdo, title e champions

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,49 @@
 import styles from './Home.module.css'
+import { Link } from 'react-router-dom'
 
-function Home (){
-    return(
-        <h1>Home aoba</h1>        
+function Home() {
+    return (
+        <>
+            <section>
+                <div className={styles.containerHeadHome}>
+                    <video className={styles.videoHomeBackground} autoPlay muted loop>
+                        <source src='src/assets/Home/Video/video_home.webm' type="video/mp4" />
+                    </video>
+
+                    <div className={styles.borderVideo}>
+
+                    <div className={styles.videoGroup}>
+                        <video autoPlay muted loop className={styles.videoHomeTitle}>
+                            <source src='src/assets/Home/Video/video_home.webm' type="video/mp4" />
+                        </video>
+                        <div className={styles.imgButtonGroup}>
+
+                            <img className={styles.imgLeague} src='/src/assets/Home/Logo/logo-1200.png' />
+
+                            <div className={styles.borderButton}>
+                                <button className={styles.buttonVideo}>
+                                    <Link to="/">Jogue de Graça</Link>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section >
+            <section>
+                <div className={styles.groupTextHomeChampion}>
+                    <p className={styles.escolhaSeu}>ESCOLHA SEU</p>
+                    <h1 className={styles.campeaoHomeh1}>CAMPEÃO</h1>
+                    <p className={styles.textPChampion}>Quer você goste de se jogar na batalha, ajudar seus aliados ou os dois, existe um lugar para você no Rift.</p>
+                    <div className={styles.groupBtnHomeChampion}>
+                        <button className={styles.btnDescubraMais}><Link to= "/champions">DESCUBRA MAIS CAMPEÕES</Link></button>
+                        <button className={styles.playNow}>JOGAR AGORA</button>
+                    </div>
+                </div>
+
+            </section>
+
+        </>
     )
 }
 
