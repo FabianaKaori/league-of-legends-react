8b1c68afa507401b3385237bdcb7a23b0460415a
FabianaKaori <fabianakaori.93@gmail.com>
2023-05-02 15:35:39 -0300

    Criação de componente Teste

---
+++ src/components/Teste/Teste.tsx
@@ -0,0 +1,11 @@
+import styles from './Teste.module.css'
+
+function Teste() {
+    return (
+        <>
+            <img className={styles.imgChampion} src='src/assets/Home/Teste/Aatrox_0.jpg' />
+        </>
+    );
+}
+
+export default Teste
