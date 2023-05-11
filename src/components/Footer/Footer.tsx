763c086c14c40402cd978aef0490e72e5b9e1d17
Fabiana Kaori <fabianakaori.93@gmail.com>
2023-05-11 00:45:11 -0300

    Correção img src

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className={styles.containerFooter}>
             <div className={styles.videoGroup}>
                 <video autoPlay muted loop className={styles.videoFooter}>
-                    <source src='src\assets\Home\Video\sylasGarenLux.mp4' type="video/mp4" />
+                    <source src='/src/assets/Home/Video/sylasGarenLux.mp4' type="video/mp4" />
                 </video>
 
                 <div className={styles.borderButton}>
@@ -20,8 +20,8 @@ function Footer() {
                 <h2>leve o league com você</h2>
                 <p>Baixe o aplicativo do League para sempre estar conectado aos seus amigos e ter acesso às partidas e notícias mais recentes dos Esports.</p>
                 <div className={styles.imgGroup}>
-                    <img className={styles.appStoreLogo} src='src\assets\Home\Logo\appStore.svg' />
-                    <img className={styles.googlePlayLogo} src='src\assets\Home\Logo\googlePlay.svg' />
+                    <img className={styles.appStoreLogo} src='/src/assets/Home/Logo/appStore.svg' />
+                    <img className={styles.googlePlayLogo} src='/src/assets/Home/Logo/googlePlay.svg' />
                 </div>
             </div>
             <ul className={styles.linkGroup}>
@@ -34,14 +34,14 @@ function Footer() {
             </ul>
             <div className={styles.finishGroup}>
                 <div className={styles.iconYoutube}>
-                    <img src='src\assets\Home\Logo\youtube.svg' title='Youtube' />
-                    <img src='src\assets\Home\Logo\twitter.svg' title='Twitter' />
-                    <img src='src\assets\Home\Logo\facebook.svg' title='Facebook' />
-                    <img src='src\assets\Home\Logo\instagram.svg' title='Instagram' />
-                    <img src='src\assets\Home\Logo\reddit.svg' title='Reddit' />
+                    <img src='/src/assets/Home/Logo/youtube.svg' title='Youtube' />
+                    <img src='/src/assets/Home/Logo/twitter.svg' title='Twitter' />
+                    <img src='/src/assets/Home/Logo/facebook.svg' title='Facebook' />
+                    <img src='/src/assets/Home/Logo/instagram.svg' title='Instagram' />
+                    <img src='/src/assets/Home/Logo/reddit.svg' title='Reddit' />
                 </div>
                 <div className={styles.riotLogoFooter}>
-                    <img src='src\assets\Home\Logo\riotGamesFooter.svg' title='Riot Games' />
+                    <img src='/src/assets/Home/Logo/riotGamesFooter.svg' title='Riot Games' />
 
                     <p>™ & © 2023 Riot Games, Inc. Todos os direitos reservados. Riot Games, League of Legends e PvP.net são marcas registradas e marcas de serviço da Riot Games, Inc.</p>
                 </div>
@@ -56,7 +56,7 @@ function Footer() {
 
                 <div className={styles.cardParentalRating}>
                     <div className={styles.parentalRating}>
-                        <img src='src\assets\Home\Logo\12.png' title='Classificação indicativa 12 anos' />
+                        <img src='/src/assets/Home/Logo/12.png' title='Classificação indicativa 12 anos' />
                         <ul>
                             <li>Violência</li>
                             <li>Conteúdo Sexual</li>
